dd9a40d43c71e6f074e27e89f877b9e44db4eef6
Pierre Martin <pierre@occitech.fr>
2015-08-20 08:59:32 +0200

    Restart the container when running it

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-docker run --name barnabe --expose 3000 -p 3000:3000 -d -v "$PWD":/usr/src/barnabe -w /usr/src/barnabe node:0.10 npm start
+docker stop barnabe && docker rm -v barnabe && docker run --name barnabe --expose 3000 -p 3000:3000 -d -v "$PWD":/usr/src/barnabe -w /usr/src/barnabe node:0.10 npm start
